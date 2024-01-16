ca710bfd76c375f2d6c3e1b2d3701b682c0bb7dd
Drew Nelson <realseal177@gmail.com>
2024-01-16 13:18:37 -0800

    blue added, render working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from '@/components/Header'
 
 export const metadata: Metadata = {
   title: 'Booking.com Clone',
@@ -15,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/* {Header} */}
+        <Header />
         
         {children}
         
